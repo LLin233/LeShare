1ccf3f156c8a91b9ba994b090b25b899c400d13e
leonard-lin <linle.job@gmail.com>
2015-05-11 01:47:15 -0700

    createMessage() create msg object contains what we need to send to target/targets

--- app/src/main/java/androidpath/ll/leshare/Helper/ParseConstants.java
+++ app/src/main/java/androidpath/ll/leshare/Helper/ParseConstants.java
@@ -4,6 +4,17 @@ package androidpath.ll.leshare.Helper;
  * Created by Le on 2015/5/8.
  */
 public class ParseConstants {
+
+    //Keys : field names
     public static final String KEY_USERNAME = "username";
     public static final String KEY_RELATION = "relation_friend";
+    public static final String KEY_RECIPIENT_IDS = "recipientIds";
+    public static final String KEY_SENDER_ID = "senderId";
+    public static final String KEY_SENDER_NAME = "senderName";
+    public static final String KEY_FILE = "file";
+    public static final String KEY_FILE_TYPE = "fileType";
+
+
+    //Class name
+    public static final String CLASS_MESSAGES = "Messages";
 }
--- app/src/main/java/androidpath/ll/leshare/View/RecipientsActivity.java
+++ app/src/main/java/androidpath/ll/leshare/View/RecipientsActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -12,10 +14,12 @@ import android.widget.TextView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import androidpath.ll.leshare.Helper.MyAlert;
@@ -31,6 +35,7 @@ public class RecipientsActivity extends AppCompatActivity {
     protected List<ParseUser> mFriends;
     protected ParseRelation<ParseUser> mFriendsRelation;
     protected ParseUser mCurrentUser;
+    protected MenuItem mMenuItem_Send;
 
     @InjectView(R.id.recipients_progressBar)
     ProgressBar mProgressBar;
@@ -45,6 +50,19 @@ public class RecipientsActivity extends AppCompatActivity {
         ButterKnife.inject(this);
         mFriendsListView.setEmptyView((TextView) findViewById(android.R.id.empty)); //if list is empty
         mFriendsListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        //add click item event
+        mFriendsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (mFriendsListView.getCheckedItemCount() > 0) {
+                    mMenuItem_Send.setVisible(true);
+                } else {
+                    mMenuItem_Send.setVisible(false);
+                }
+
+            }
+        });
     }
 
     @Override
@@ -85,6 +103,7 @@ public class RecipientsActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_recipients, menu);
+        mMenuItem_Send = menu.getItem(0);
         return true;
     }
 
@@ -96,10 +115,34 @@ public class RecipientsActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_send) {
+            //TODO send message to back-end
+            ParseObject message = createMessgae();
+            //send(message);
+            Log.i(TAG, "Send msg");
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    private ParseObject createMessgae() {
+        ParseObject message = new ParseObject(ParseConstants.CLASS_MESSAGES);
+        message.put(ParseConstants.KEY_SENDER_ID, ParseUser.getCurrentUser().getObjectId());
+        message.put(ParseConstants.KEY_USERNAME, ParseUser.getCurrentUser().getUsername());
+        message.put(ParseConstants.KEY_RECIPIENT_IDS, getRecipientIds());
+
+        return message;
+
+    }
+
+    private ArrayList<String> getRecipientIds() {
+        ArrayList<String> recipientIds = new ArrayList<String>();
+        for (int i = 0; i < mFriendsListView.getCount(); i++) {
+            if (mFriendsListView.isItemChecked(i)) { // the one who user decide to send msg to
+                recipientIds.add(mFriends.get(i).getObjectId());
+            }
+        }
+        return recipientIds;
+    }
+
 }
