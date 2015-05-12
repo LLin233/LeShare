a9b99ac45542b5567c1f3fb89610f4192a640b42
leonard-lin <linle.job@gmail.com>
2015-05-12 14:44:30 -0700

    KEY_SENDER_NAME should be "username" in parse back-end

--- app/src/main/java/androidpath/ll/leshare/Helper/ParseConstants.java
+++ app/src/main/java/androidpath/ll/leshare/Helper/ParseConstants.java
@@ -10,9 +10,10 @@ public class ParseConstants {
     public static final String KEY_RELATION = "relation_friend";
     public static final String KEY_RECIPIENT_IDS = "recipientIds";
     public static final String KEY_SENDER_ID = "senderId";
-    public static final String KEY_SENDER_NAME = "senderName";
+    public static final String KEY_SENDER_NAME = "username";
     public static final String KEY_FILE = "file";
     public static final String KEY_FILE_TYPE = "fileType";
+    public static final String KEY_CREATED_AT = "createdAt";
 
     //file name
     public static final String TYPE_IMAGE = "image";
